1fa8946143fccd9cceaa3c9da0a262c863e4c367
Amit8127 <amitmonu0000@gmail.com>
2023-03-12 14:22:20 +0530

    new push

---
+++ sum of numbers/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int sum  = 0;
+        for(int i = 0; i < n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+        scn.close();
+    }
+}
